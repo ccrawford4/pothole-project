77bfd3089093eb6a7b8b9e88e8fd890a068cd859
ccrawford4 <calumcrawford9@gmail.com>
2024-04-05 23:08:03 -0700

    testing workflow again

--- app/myapp/views.py
+++ app/myapp/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse("Hello, World! TEST")
+    return HttpResponse("Hello, World! TEST Is this workflow actually working")
 
